9cad268d8a2414e94cfccd2acac2219979b31f00
Erika Hunter <erika22hunter@outlook.com>
2018-09-29 17:15:23 -0600

    add text and photo

--- web/homepage.php
+++ web/homepage.php
@@ -1,12 +1,28 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
+        <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1">
         <link rel = "stylesheet" type="text/css" href="homepage-styles.css">
+        <link rel = "stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     </head>
     <body>
         <?php
         echo "<h2>National Parks</h2>";
         ?>
+        <div class ='containter'>
+            <div class="row">
+                <div class="col-lg-4">
+                    <h3>Yellowstone National Park</h3>
+                        <p>Yellowstone National Park is a nearly 3,500-sq.-mile wilderness recreation area atop a volcanic hot spot. 
+                            Mostly in Wyoming, the park spreads into parts of Montana and Idaho too. 
+                            Yellowstone features dramatic canyons, alpine rivers, lush forests,
+                             hot springs and gushing geysers, including its most famous, Old Faithful. 
+                             It's also home to hundreds of animal species, including bears, wolves, bison, elk and antelope.</p>
+                </div>
+                <div class="col-lg-8">
+                <img src="C:\Users\Erika Hunter\Documents\web-eng\cs313-php\images\YNP_hero1_v2.gif" alt="Italian Trulli">
+                </div>
+        </div>
         
 
     </body>
