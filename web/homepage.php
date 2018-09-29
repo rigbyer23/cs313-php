86d99f5f1d4028af19836b8c62eb2d763200e598
Erika Hunter <erika22hunter@outlook.com>
2018-09-29 16:14:26 -0600

    add homepage app and css

---
+++ web/homepage.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel = "stylesheet" type="text/css" href="homepage-styles.css">
+    </head>
+    <body>
+        <?php
+        echo "<h2>National Parks</h2>";
+        ?>
+        
+
+    </body>
+</html>
