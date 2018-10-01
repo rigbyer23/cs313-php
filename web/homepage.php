96ec941635c2000410b937f3dafdbeebcf6ef687
Erika Rigby <rigbyerika@gmail.com>
2018-10-01 08:09:12 -0600

    Add fantastic fonts to headers

--- web/homepage.php
+++ web/homepage.php
@@ -7,6 +7,7 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+        <link href="https://fonts.googleapis.com/css?family=Amatic+SC:400,700" rel="stylesheet">
     </head>
     <body>
     <div class ='containter'>
