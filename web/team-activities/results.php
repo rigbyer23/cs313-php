a6bdfe84495d3082064240078ecc7ff762ebc3d9
Erika Rigby <rigbyerika@gmail.com>
2018-10-02 16:31:33 -0600

    updates1

--- web/team-activities/results.php
+++ web/team-activities/results.php
@@ -1,7 +1,7 @@
 <html>
     <head>
         <body>
-            <p>What's up <?php echo $_POST["name"]; ?></p>
+            <p>What's up <?php echo $_POST["firstName"]; ?></p>
             <p>Your email is: <a href="mailto:<?php echo $_POST["email"]; ?>"><?php echo $_POST["email"]; ?></a></p>
             <p>Your major is: <?php echo $_POST["major"]; ?></p>
             <p>Your comments were: <?php echo $_POST["comment"]; ?></p>
