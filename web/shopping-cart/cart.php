d0e20de3bfe21ea514abdfe63bde7e0980063f91
Erika Rigby <rigbyerika@gmail.com>
2018-10-05 15:05:47 -0600

    array index fix

--- web/shopping-cart/cart.php
+++ web/shopping-cart/cart.php
@@ -3,9 +3,9 @@
         <body>
             <h1>Your Cart</h1>
             <ul>
-            <li><?php echo $_POST["gear"]; ?></li>
-            <li><?php echo $_POST["gear"]; ?></li>
-            <li><?php echo $_POST["gear"]; ?></li>
+            <li><?php echo $_POST["gear[$item]"]; ?></li>
+            <li><?php echo $_POST["gear[$item]"]; ?></li>
+            <li><?php echo $_POST["gear[$item]"]; ?></li>
         </ul>
 
             <?php if(isset($_POST['gear']))
