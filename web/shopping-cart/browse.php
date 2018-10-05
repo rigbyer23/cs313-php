a2eecb030bd1d07f16fd6840883c833186c51257
Erika Rigby <rigbyerika@gmail.com>
2018-10-05 13:19:40 -0600

    bootstrap navbar

--- web/shopping-cart/browse.php
+++ web/shopping-cart/browse.php
@@ -2,16 +2,40 @@
     <head>
       <title>Take a Hike</title>
       <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1">
-      <link rel = "stylesheet" type="text/css" href="cart.css">
         <link rel = "stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+        <link rel = "stylesheet" type="text/css" href="cart.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
         <link href="https://fonts.googleapis.com/css?family=Amatic+SC:400,700" rel="stylesheet">
     </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-light bg-primary">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
 
-<div class ='containter'>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+<!-- <div class ='containter'>
     <div class="row">
         <div class ="header col-lg-12">
         
@@ -23,7 +47,7 @@
         
         </div>
     </div>
-</div>
+</div> -->
 </body>
 </html>
 
