dd9b11add0e4d0711b398ab0e464efb39b2c9891
Erika Rigby <rigbyerika@gmail.com>
2018-10-05 11:04:22 -0600

    added closing tags

--- web/homepage.php
+++ web/homepage.php
@@ -2,12 +2,12 @@
 <html lang="en">
     <head>
         <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1">
-        <link rel = "stylesheet" type="text/css" href="homepage-styles.css">
-        <link href="https://fonts.googleapis.com/css?family=Amatic+SC:400,700" rel="stylesheet">
         <link rel = "stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+        <link rel = "stylesheet" type="text/css" href="homepage-styles.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+        <link href="https://fonts.googleapis.com/css?family=Amatic+SC:400,700" rel="stylesheet">
         
     </head>
     <body>
--- web/shopping-cart/browse.php
+++ web/shopping-cart/browse.php
@@ -2,8 +2,8 @@
     <head>
       <title>Take a Hike</title>
       <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1">
-        <link rel = "stylesheet" type="text/css" href="cart.css">
         <link rel = "stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+        <link rel = "stylesheet" type="text/css" href="cart.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
@@ -23,10 +23,9 @@
         
         </div>
     </div>
-   
-    <div class="row">
-
-    </div>
+</div>
+</body>
+</html>
 
 
 
