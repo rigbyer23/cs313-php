9230c5ab9585ad18d50e7a98da80c339ebff4c68
Erika Rigby <rigbyerika@gmail.com>
2018-10-08 11:56:50 -0600

    Move buttons

--- web/shopping-cart/cart.php
+++ web/shopping-cart/cart.php
@@ -52,7 +52,7 @@
             <?php
                 foreach($_SESSION["gear"] as $item){
                     echo $item." ";
-                    echo "<button type='submit' name='delete' value='$item'>Remove</button><br><hr>";
+                    echo "<button type='submit' name='delete' value='$item' style='justify-content:flex-end;'>Remove</button><br><hr>";
                 }
                 ?>
                 
