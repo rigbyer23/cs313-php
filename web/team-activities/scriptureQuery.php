d192cb8ff089183ab2635ea5149c745b73b084b7
Erika Rigby <rigbyerika@gmail.com>
2018-10-16 16:26:24 -0600

    added scripture db and php

--- web/team-activities/scriptureQuery.php
+++ web/team-activities/scriptureQuery.php
@@ -1,5 +1,8 @@
 <?php
 require("dbconnect.php")
+if(isset($_POST['book'])) {
+    $book = $_POST['book'];
+};
 ?>
 
 <!DOCTYPE html>
@@ -11,13 +14,27 @@ require("dbconnect.php")
     <body>
     <h1>Scripture Resources</h1>
         <?php
-        foreach ($db->query('SELECT book, chapter, verse, content FROM scriptures') as $row)
+        if(!isset($book)) {
+            $query = 'SELECT book, chapter, verse, content FROM scriptures WHERE book = $book';
+        } else {
+            $query = 'SELECT book, chapter, verse, content FROM scriptures';
+        }
+        foreach ($db->query($query) as $row)
         {
           echo '<b>'.$row['book'] ;
           echo ' '.$row['chapter'].':';
           echo $row['verse'].'</b>';
           echo ' -"'.$row['content'].'"<br>';
         }
+        ?>
+
+        <form action="scriptureQuery.php" method="GET">
+            Book: <input type="text" name="book"><br/>
+            <input type="submit">
+        </form>
+
+        <?php
+
         ?>
     </body>
 
