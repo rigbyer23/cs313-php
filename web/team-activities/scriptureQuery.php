45687be618aab8571acd20d1be2616ac14e8984a
Erika Rigby <rigbyerika@gmail.com>
2018-10-16 16:09:26 -0600

    added scripture db and php

--- web/team-activities/scriptureQuery.php
+++ web/team-activities/scriptureQuery.php
@@ -9,13 +9,14 @@ require("dbconnect.php")
     </head>
 
     <body>
+    <h1>Scripture Resources</h1>
         <?php
         foreach ($db->query('SELECT book, chapter, verse, content FROM scriptures') as $row)
         {
           echo '<b>'.$row['book'] ;
           echo ' '.$row['chapter'].':';
           echo $row['verse'].'</b>';
-          echo ' -\"'.$row['content'].'\"';
+          echo ' -"'.$row['content'].'"<br>';
         }
         ?>
     </body>
