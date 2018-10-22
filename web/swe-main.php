30fc6a1a3e4f25d53c4553401d10178dd1c1ff2d
Erika Rigby <rigbyerika@gmail.com>
2018-10-22 08:31:48 -0600

    testing php

--- web/swe-main.php
+++ web/swe-main.php
@@ -2,15 +2,12 @@
 //connect to db
 require("./team-activities/dbconnect.php");
 $db = get_db();
- if(isset($_POST['member']))
+ if(isset($_GET['last_name']))
  {
-    $member = $_POST['member'];
-    $stmt = $db->prepare('SELECT first_name, last_name, email FROM member WHERE last_name = $last_name');
+    $last_name = $_GET['last_name'];
+    $stmt = $db->prepare('SELECT first_name, last_name, email FROM member WHERE last_name = :last_name');
+    $stmt->bindValue(':last_name', $last_name, PDO::PARAM_STR);
     $stmt->execute();
-    } else {
-        $stmt->execute();
-        $members = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        
     };
 ?>
 
@@ -21,19 +18,18 @@ $db = get_db();
          <body>
             <h1>Society of Women Engineers</h1>
                 <form action="swe-main.php" method="GET">
-                Members <input type="text" name="member"><br/>
-                 <ul>
-                    <?php
-                        foreach ($db->stmt($stmt) as $member){
-                            $first_name =$member['first_name'];
-                            $last_name = $member['last_name'];
-                            $email = $member['email'];
-                            echo "<li><p>$first_name $last_name $email</p></li>";
+                Members <input type="text" name="last_name"><br/>
+                <input type="submit" value="Search">
+                </form>
+
+                <ul>
+                <?php
+                        foreach ($stmt->fetchAll() as $row){
+                           
+                            echo $row['first_name']. $row['last_name']. $row['email'];
                         }
                     ?>
                 </ul>
-                <input type="submit" value="Search">
-                </form>
 
                 <form action="swe-main.php" method="GET">
                 Advisory Board Members <input type="text" name="book"><br/>
