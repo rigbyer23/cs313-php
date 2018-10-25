96d65706712dfa50a416bc2369c18a9233fdc176
Erika Rigby <rigbyerika@gmail.com>
2018-10-25 16:13:19 -0600

    Courses added

---
+++ web/team-activities/courses.php
@@ -0,0 +1,41 @@
+<?php
+ require("./team-activities/dbconnect.php");
+$db = get_db();
+ 
+$query ='SELECT id, code, name FROM course';
+
+$stmt = $db->prepare($query);
+$stmt->execute();
+$courses = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+
+<html>
+    <head>
+    <title>Movies</title>
+    </head>
+
+        <body>
+            <h1>Movies</h1>
+            
+            <ul>
+
+            <?php
+                foreach ($courses as $course){
+                    $id = $course['id'];
+                    $name = $course['name'];
+                    $code = $course['code'];
+
+                    echo "<li><p>$code - $name</p></li>\n";
+                }
+            
+            ?>
+                <li><p> CS 313 - Web II</p></li>
+                <li><p> CS 313 - Web II</p></li>
+                <li><p> CS 313 - Web II</p></li>
+                <li><p> CS 313 - Web II</p></li>
+            </ul>
+
+
+
+</html>
