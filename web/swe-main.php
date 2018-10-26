b174b0f354302f1e4040570776a70cf6d05cd7f6
Erika Rigby <rigbyerika@gmail.com>
2018-10-25 22:09:21 -0600

    remove image

--- web/swe-main.php
+++ web/swe-main.php
@@ -64,13 +64,11 @@ else if(isset($_GET['see_all']))
          <body>
          <div class="container">
             <div class="row">
-                <div class="col-lg-4" style="background-color: rgb(131, 90, 141) height:60px;">
-                    <h1 style="font-family: 'Pacifico', cursive; color: white;">BYUI<br>SWE</h1>
-                     </div>
-                <div class="col-lg-8">
-                     <img src="./engineer-girl.jpg" class="image-fluid float-right" alt ="Girl engineer">
+             
+                <div class="col-lg-12" style="background-color: rgb(131, 90, 141);">
+                    <h1 style="font-family: 'Pacifico', cursive; color: white;">BYUI-SWE</h1>
                 </div>
-            
+            </div>
 
             <div class="row">
             <div class="col-lg-4">
