06f22c717a7c1d84bc6dc092d5cf46bb4037a701
Erika Rigby <rigbyerika@gmail.com>
2018-10-25 20:56:13 -0600

    added background photo

--- web/swe-main.php
+++ web/swe-main.php
@@ -64,9 +64,10 @@ else if(isset($_GET['see_all']))
          <body>
          <div class="container">
             <div class="row">
-                <div class="col-lg-12" style="background-color: rgb(131, 90, 141); background-image: url('./engineer-girl.jpg')">
+                <div class="col-lg-4" style="background-color: rgb(131, 90, 141);">
                     <h1 style="font-family: 'Pacifico', cursive; color: white;">BYUI-SWE</h1>
                 </div>
+                <div class="col-lg-8" style="background-image: url('./engineer-girl.jpg'); background-repeat: cover;">
             </div>
 
             <div class="row">
