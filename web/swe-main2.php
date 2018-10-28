8f942224b10339bfcc8188ec2301da48f5226ab1
Erika Rigby <rigbyerika@gmail.com>
2018-10-27 23:24:57 -0600

    Attempt to add img

--- web/swe-main2.php
+++ web/swe-main2.php
@@ -65,9 +65,9 @@ else if(isset($_GET['see_all']))
          <div class="container">
             <div class="row">
              
-                <div class="col-lg-12" style="background-color: rgb(90, 82, 119);">
-                    <h1 style="font-family: 'Pacifico', cursive; color: white;">BYUI-SWE</h1>
-                    <img class="img-responsive float-right width: 50%; height:50%;" src="./swe-image.jpg">
+                <div class="col-lg-12 d-flex p-2" style="background-color: rgb(90, 82, 119);">
+                      <div class="mr-auto p-2"><h1 style="font-family: 'Pacifico', cursive; color: white;">BYUI-SWE</h1></div>
+                    <div class="p-2"><img class="img-responsive" style="object-fit: cover; height:50%; width:75%;" src="./swe-image.jpg"></div>
                 </div>
             </div>
             <br>
