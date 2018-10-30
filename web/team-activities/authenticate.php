c8aaaafe511df2675c48b369c6605b25974df308
Erika Rigby <rigbyerika@gmail.com>
2018-10-30 16:42:51 -0600

    adding team activity

--- web/team-activities/authenticate.php
+++ web/team-activities/authenticate.php
@@ -2,8 +2,18 @@
 $password = $_POST['password'];
 $username = $_POST['username'];
 
-$query = $db->prepare('SELECT password FROM user WHERE username = $username;');
-password_verify($password);
+$query = $db->prepare('SELECT password FROM user WHERE username = :username;');
+
+$query->bindValue(':username', $username, PDO::PARAM_STR);
+$query->execute();
+
+$realPass = $query->fetch(PDO::FETCH_ASSOC);
+
+var_dump($realPass);
+
+if (password_verify($password, $realPass['password'])){
+    
+}
 
 
 ?>
