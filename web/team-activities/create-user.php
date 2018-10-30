1d8b1f7dc0a8ed9c0ebd1f9353c326fd5c33c65e
Erika Rigby <rigbyerika@gmail.com>
2018-10-30 16:54:54 -0600

    adding team activity

--- web/team-activities/create-user.php
+++ web/team-activities/create-user.php
@@ -3,6 +3,7 @@
  require("./dbconnect.php");
  $db = get_db();
 $password = htmlspecialchars($_POST['password']);
+$username = htmlspecialchars($_POST['username']);
 $hashpass = password_hash($password, PASSWORD_DEFAULT);
 
     $someQuery = $db->prepare('INSERT INTO user (username, password) VALUES
