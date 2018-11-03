3c41ddb18a5b8f9b35f352edf4dbee46edb2ebc6
Erika Rigby <rigbyerika@gmail.com>
2018-11-03 17:13:40 -0600

    working on inserts

--- web/swe-app/insertMember.php
+++ web/swe-app/insertMember.php
@@ -1,12 +1,8 @@
 <?php
 require("./membersModol.php");
-var_dump($_POST['firstNcol']);
-if(isset($_POST['addMember'])){
- 
-    $fname = htmlspecialchars($_POST['firstNcol']);
-      
+
+    $fname = htmlspecialchars($_POST['firstNcol']); 
     $lname = htmlspecialchars($_POST['secNcol']);
-    
     $email = htmlspecialchars($_POST['emailCol']);
     $phone = htmlspecialchars($_POST['phoneCol']);
     $major = htmlspecialchars($_POST['majorCol']);
@@ -14,16 +10,16 @@ if(isset($_POST['addMember'])){
     $person = insertMember($fname, $lname, $email, $phone, $major);
     var_dump($person);
    
-}
 
-if(isset($_POST['addBoard'])){
-    $kind = $_POST['addBoard'];
-    $person = insertPeople($kind);
-}
 
-if(isset($_POST['addSpeaker'])){
-    $kind = $_POST['addSpeaker'];
-    $person = insertPeople($kind);
-}
+// if(isset($_POST['addBoard'])){
+//     $kind = $_POST['addBoard'];
+//     $person = insertPeople($kind);
+// }
+
+// if(isset($_POST['addSpeaker'])){
+//     $kind = $_POST['addSpeaker'];
+//     $person = insertPeople($kind);
+// }
 //  header('location: ./memberListView.php?membersRadio=allMembers');
 ?>
