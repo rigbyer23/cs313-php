aeb553d7424301af251566c0e8800ef55d3bf513
Erika Rigby <rigbyerika@gmail.com>
2018-11-05 23:07:13 -0700

    ab member table

--- web/swe-app/insertMember.php
+++ web/swe-app/insertMember.php
@@ -38,7 +38,7 @@ else {
 
     $speaker = insertSpeaker($fullName,$title,$spEmail,$sphone); 
  
-    // header('location: ./memberListView.php?speakers=allSpeakers');
+    header('location: ./memberListView.php?speakers=allSpeakers');
 }
 
 // if(isset($_POST['addBoard'])){
