b14dd28243d19c6245f01e33bff4deaea71a877e
Erika Rigby <rigbyerika@gmail.com>
2018-11-05 22:15:35 -0700

    add id's to table

--- web/swe-app/deleteMember.php
+++ web/swe-app/deleteMember.php
@@ -3,14 +3,13 @@ require("../team-activities/dbconnect.php");
 $db = get_db();
 var_dump($_GET['id']);
 // if (isset($_GET['id']))
-if(isset($_POST['id'])){
-    var_dump($_POST);
-    var_dump($_POST['id']); 
-    $id = $_POST['id'];
+if(isset($_GET['id'])){
+    $id = $_GET['id'];
     $someQuery = $db->prepare("DELETE FROM member m WHERE m.id = :id");
     $someQuery->bindValue(":id", $id, PDO::PARAM_INT);
     $someQuery->execute();
-    //  header('location: ./memberListView.php?membersRadio=allMembers');
+    header('location: ./memberListView.php?membersRadio=allMembers');
+    die();
 }
 
 if(isset($_POST['deleteBoardM'])){
