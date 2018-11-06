8ba7a50de37711f5eebd1a87e3ac746b860e64d5
Erika Rigby <rigbyerika@gmail.com>
2018-11-06 01:41:17 -0700

    Make another query

--- web/swe-app/swe-create.php
+++ web/swe-app/swe-create.php
@@ -13,18 +13,20 @@ $username = htmlspecialchars($_POST['username']);
 $hashpass = password_hash($password, PASSWORD_DEFAULT);
 
     $someQuery = $db->prepare('INSERT INTO ab_member (username, password, position, exp_date) VALUES
-        (:username, :password, :position, :exp_date) INSERT INTO member(first_name, last_name, email, phone,)VALUES(:first_name, :last_name, :email, :phone)');
+        (:username, :password, :position, :exp_date)');
+    $otherQuery = $db->prepare('INSERT INTO member(first_name, last_name, email, phone,)VALUES(:first_name, :last_name, :email, :phone)');
 
-    $someQuery->bindValue(":first_name", $firstName, PDO::PARAM_STR);
-    $someQuery->bindValue(":last_name", $lastName, PDO::PARAM_STR);
+    $otherQuery->bindValue(":first_name", $firstName, PDO::PARAM_STR);
+    $otherQuery->bindValue(":last_name", $lastName, PDO::PARAM_STR);
     $someQuery->bindValue(":username", $username, PDO::PARAM_STR);
     $someQuery->bindValue(":password", $hashpass, PDO::PARAM_STR);
     $someQuery->bindValue(":position", $position, PDO::PARAM_STR);
     $someQuery->bindValue(":exp_date", $exp_date, PDO::PARAM_STR);
-    $someQuery->bindValue(":email", $email, PDO::PARAM_STR);
-    $someQuery->bindValue(":phone", $phone, PDO::PARAM_STR);
+    $otherQuery->bindValue(":email", $email, PDO::PARAM_STR);
+    $otherQuery->bindValue(":phone", $phone, PDO::PARAM_STR);
 
     $someQuery->execute();
+    $otherQuery->execute();
 
    
     header("location: ./swe-signin.php");
