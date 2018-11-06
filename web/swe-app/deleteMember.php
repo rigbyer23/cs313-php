d2f29d2f40abc708a8e0eeb2921f5d5bfaa5d8a8
Erika Rigby <rigbyerika@gmail.com>
2018-11-05 17:01:56 -0700

    delete button

--- web/swe-app/deleteMember.php
+++ web/swe-app/deleteMember.php
@@ -2,7 +2,9 @@
 require("../team-activities/dbconnect.php");
 $db = get_db();
 if(isset($_POST['Remove'])){ 
+    var_dump($_POST['Remove']);
     $someQuery = $db->prepare('DELETE FROM member m WHERE m.id =' .$_POST['deleteMember']);
+    echo $someQuery;
     $someQuery->execute();
      header('location: ./memberListView.php?membersRadio=allMembers');
 }
