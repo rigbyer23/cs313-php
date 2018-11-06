a5d20ff1adfde8269cf4fe18785150d8b355b894
Erika Rigby <rigbyerika@gmail.com>
2018-11-06 11:57:12 -0700

    memberID troubleshoot

--- web/swe-app/swe-create.php
+++ web/swe-app/swe-create.php
@@ -13,25 +13,33 @@ $password = htmlspecialchars($_POST['password']);
 $username = htmlspecialchars($_POST['username']);
 $hashpass = password_hash($password, PASSWORD_DEFAULT);
 
+   
     $otherQuery = $db->prepare('INSERT INTO member(first_name, last_name, email, phone, major_id)VALUES(:first_name, :last_name, :email, :phone, :major_id)');
+    $otherQuery->bindValue(":first_name", $firstName, PDO::PARAM_STR);
+    $otherQuery->bindValue(":last_name", $lastName, PDO::PARAM_STR);
+    $otherQuery->bindValue(":email", $email, PDO::PARAM_STR);
+    $otherQuery->bindValue(":phone", $phone, PDO::PARAM_STR);
+    $otherQuery->bindValue(":major_id", $major, PDO::PARAM_INT);
+
+       $otherQuery->execute(); 
+
     $memberID = $db->lastInsertId();
     var_dump($memberID);
+
+
     $someQuery = $db->prepare('INSERT INTO ab_member (username, password, '.$memberID.' position, exp_date) VALUES
         (:username, :password, :memberID, :position, :exp_date)');
     
   
-    $otherQuery->bindValue(":first_name", $firstName, PDO::PARAM_STR);
-    $otherQuery->bindValue(":last_name", $lastName, PDO::PARAM_STR);
+   
     $someQuery->bindValue(":username", $username, PDO::PARAM_STR);
-    $otherQuery->bindValue(":major_id", $major, PDO::PARAM_INT);
     $someQuery->bindValue(":password", $hashpass, PDO::PARAM_STR);
     $someQuery->bindValue(":position", $position, PDO::PARAM_STR);
     $someQuery->bindValue(":memberID", $memberID, PDO::PARAM_STR);
     $someQuery->bindValue(":exp_date", $exp_date, PDO::PARAM_INT);
-    $otherQuery->bindValue(":email", $email, PDO::PARAM_STR);
-    $otherQuery->bindValue(":phone", $phone, PDO::PARAM_STR);
+  
 
-    $otherQuery->execute(); 
+ 
     $someQuery->execute();
         
 
